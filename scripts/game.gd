77d136b79daa233c8de52b969ce03c47fcffdb82
NeyZzO <66218417+NeyZzO@users.noreply.github.com>
2025-02-02 01:04:37 +0100

    Cave generation 1st version

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,12 +1,14 @@
 extends Node2D
 
-var noise: FastNoiseLite = FastNoiseLite.new();
+var surface_noise: FastNoiseLite = FastNoiseLite.new();
+var cave_noise: FastNoiseLite = FastNoiseLite.new();
+
 @onready var map: TileMap = $TileMap;
 @onready var player: Player = $Player;
 @onready var hud: CanvasLayer = $HUD;
 
 @export var MAP_WIDTH: int = 700;
-@export var MAP_HEIGHT: int = 120;
+@export var MAP_HEIGHT: int = 300;
 
 const GRASS: Vector2i = Vector2i(0, 0);
 const DIRT: Vector2i = Vector2i(1, 0);
@@ -16,18 +18,36 @@ var heights: Array[int] = [];
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	randomize();
-	noise.seed = randi();
-	
-	noise.noise_type = FastNoiseLite.TYPE_PERLIN;
-	noise.frequency = 0.01; 
-	print("Using seed : ", noise.seed)
+	var SEED: int = randi();
+	surface_noise.seed = SEED;
+	surface_noise.noise_type = FastNoiseLite.TYPE_PERLIN;
+	surface_noise.frequency = 0.01; 
 	draw_map();
+
 	# Place the player at the center of the map.
 	print(heights[MAP_WIDTH/2])
 	player.position = Vector2(0 + 8, (heights[MAP_WIDTH/2] * 16) - 8);
 
+	# Now generate the caves;
+	cave_noise.seed = SEED;
+	cave_noise.noise_type = FastNoiseLite.TYPE_PERLIN;
+	cave_noise.frequency = 0.05;
+	generateCaves();
+
+
+
+func generateCaves() -> void:
+	for x in range(-MAP_WIDTH/2, MAP_WIDTH/2):
+		for y in range(MAP_HEIGHT):
+			var value: float = cave_noise.get_noise_2d(x, y);
+			if value > 0.2:
+				print('Cave')
+				map.set_cell(0, Vector2i(x, y), 0, Vector2i(-1, -1), 0);
+
+	
+
 func getHeight(x: int) -> int:
-	return int(noise.get_noise_1d(x) * 25 + 50);
+	return int(surface_noise.get_noise_1d(x) * 25 + 50);
 
 func draw_map() -> void:
 	# First, we use a 1D noise to generate the height of the terrain
